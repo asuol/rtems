1245c2b6434cbf1e85eee2f04b8ee8fb67cb8b44
asuol <andre.lousa.marques@gmail.com>
2014-06-09 16:34:59 +0100

    Implemented rtems_gpio_input_mode

--- c/src/lib/libbsp/arm/raspberrypi/gpio/libgpio.c
+++ c/src/lib/libbsp/arm/raspberrypi/gpio/libgpio.c
@@ -4,11 +4,17 @@
 
 #include <stdio.h>
 
-#define GPIO_READ(g) *(gpio_regs_basegpio+13) &= (1<<(g))
-
 static volatile unsigned int *gpio_regs_base = (unsigned int *)BCM2835_GPIO_REGS_BASE;
 
 static bool is_initialized = false;
+
+static void arm_delay (int cycles)
+{
+  int i;
+
+  for (i = 0; i < cycles; i++)
+    asm volatile ("nop");
+}
 
 void rtems_gpio_initialize (void)
 {
@@ -78,6 +84,49 @@ int rtems_gpio_setup_pin (int pin, rtems_pin_t type)
   }
 
   gpio_pin[pin-1].pin_type = type;
+
+  return 0;
+}
+
+int rtems_gpio_input_mode (int pin, rtems_multiio_input_mode_t mode)
+{
+
+  // check current mode first
+
+  // add bitmask mode
+
+  /* Set control signal */
+  switch (mode)
+  {
+    case PULL_UP:
+      BCM2835_REG (BCM2835_GPIO_GPPUD) = (1<<1);
+      break;
+
+    case PULL_DOWN:
+      BCM2835_REG (BCM2835_GPIO_GPPUD) = (1<<0);
+      break;
+
+    case NO_PULL_RESISTOR:
+      BCM2835_REG (BCM2835_GPIO_GPPUD) = 0;
+      break;
+
+    default:
+      return -1;
+  }
+
+  /* Wait 150 cyles, as per BCM2835 documentation */
+  arm_delay (150);
+
+  /* Setup clock for the control signal */
+  BCM2835_REG (BCM2835_GPIO_GPPUDCLK0) = (1<<pin);
+
+  arm_delay (150);
+
+  /* Remove the control signal */
+  BCM2835_REG (BCM2835_GPIO_GPPUD) = 0;
+
+  /* Remove the clock */
+  BCM2835_REG (BCM2835_GPIO_GPPUDCLK0) = 0;
 
   return 0;
 }
