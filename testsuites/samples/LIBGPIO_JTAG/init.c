ee4d395d3c69de1f42d4dd2fd56615824eca5da3
Andre Marques <andre.lousa.marques@gmail.com>
2015-06-21 23:20:38 +0100

    Updated test cases

--- testsuites/samples/LIBGPIO_JTAG/init.c
+++ testsuites/samples/LIBGPIO_JTAG/init.c
@@ -5,11 +5,9 @@
 #include <rtems/test.h>
 
 #include <bsp.h> /* for device driver prototypes */
+#include <bsp/rpi-gpio.h>
 
-#include <bsp/gpio.h> /* Calls the BSP gpio library */
-#include <rtems/status-checks.h>
-
-#include <stdio.h>
+#include <assert.h>
 #include <stdlib.h>
 
 /* forward declarations to avoid warnings */
@@ -21,25 +19,19 @@ rtems_task Init(
   rtems_task_argument ignored
 )
 {
-  int rv = 0;
+  rtems_status_code sc;
 
   rtems_test_begin ();
   
   /* Initializes the GPIO API */
-  gpio_initialize ();
+  rtems_gpio_initialize ();
 
-  /* Disables the setup pins internal pull resistor */
-  int pins[] = {4,22,24,25,27};
-
-  rv = gpio_setup_input_mode (pins, 5, NO_PULL_RESISTOR);
-  RTEMS_CHECK_RV ( rv, "gpio_setup_input_mode");
-  
   /* Setups the JTAG pin interface */
-  rv = gpio_select_jtag ();
-  RTEMS_CHECK_RV ( rv, "gpio_setup_config");
+  sc = gpio_select_jtag ();
+  assert(sc == RTEMS_SUCCESSFUL);
   
-  rtems_test_end ();
-  exit ( 0 );
+  rtems_test_end();
+  exit(0);
 }
 
 #define CONFIGURE_APPLICATION_DOES_NOT_NEED_CLOCK_DRIVER
